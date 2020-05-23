adc51ec56787138bf259136995b8e2761e768437
MaksymY <maksym.yankivskyypro@gmail.com>
2020-05-23 14:51:03 +0200

    adding signals for Question model slug

---
+++ core/utils.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+ALPHANUMERIC_CHARS = string.ascii_lowercase + string.digits
+STRING_LENGTH = 6
+
+def generation_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
+    return "".join(random.choice(chars) for _ in range(length))
