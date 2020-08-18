d96f6dbcf77b350ebaf8c7fbc8912a6bb7da28d1
MaksymY <maksym.yankivskyypro@gmail.com>
2020-08-19 00:26:48 +0200

    adding cors

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
 
     'users',
     'questions',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'QuestionTime.urls'
@@ -163,3 +167,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
